a049333425a3d72ffef548a98a28b0d612c7ecf2
rocky-191 <1375022007@qq.com>
2019-03-19 09:50:14 +0800

    修改主路由名称

--- src/view/main/index.js
+++ src/view/main/index.js
@@ -3,7 +3,7 @@ import { Switch, Route } from 'react-router-dom';
 import Home from '../home/index';//首页
 import Dynamic from '../dynamic/index';//动态
 import Topic from '../topic/index';//话题
-import Brouchure from '../brochure/index';//小册
+import Brochure from '../brochure/index';//小册
 import Activity from '../activity/index';//活动
 
 class Main extends Component {
@@ -19,7 +19,7 @@ class Main extends Component {
                     <Route path='/timeline' component={Home}/>
                     <Route path='/dynamic' component={Dynamic}/>
                     <Route path='/topic' component={Topic}/>
-                    <Route path='/brochure' component={Brouchure}/>
+                    <Route path='/brochure' component={Brochure}/>
                     <Route path='/activity' component={Activity}/>
                 </Switch>
             </div>
