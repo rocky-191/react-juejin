30664d3db8634afddc6efc4debbca128a9144958
rocky-191 <1375022007@qq.com>
2019-04-02 14:29:11 +0800

    话题页内容

---
+++ src/view/topic/TopicItem.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import {NavLink} from 'react-router-dom';
+import { Badge,Avatar } from 'antd';
+import './topic.less'
+
+function TopicItem({item}){
+    const {id,topicName,topicImage,topicCount,followedNum,topicNum,isFollowed}=item;
+    return (
+        <div className='topicItem'>
+            <Badge count={topicCount} overflowCount={999}>
+                <Avatar shape="square" size={72} src={topicImage} />
+            </Badge>
+            <div style={{marginLeft:'15px'}}>
+                <div>
+                    <NavLink to={`/topic/${id}`}>{topicName}</NavLink>
+                </div>
+                <div style={{color:'#8a9aa9',marginTop:'5px'}}>
+                    <span>{followedNum}关注</span>·
+                    <span>{topicNum}沸点</span>
+                </div>
+                <div className={isFollowed?'hasFollowed':'noFollow'}>
+                    {isFollowed?'已关注':'+关注'}
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default TopicItem;
--- src/view/topic/index.js
+++ src/view/topic/index.js
@@ -1,16 +1,52 @@
 import React, { Component } from 'react';
+import TopicItem from './TopicItem';
 import './topic.less';
 
 class Topic extends Component {
     constructor(props) {
         super(props);
-        this.state = {  }
+        this.state = {
+            topicList:[
+                {
+                    id:'000001',
+                    topicName:'你怎么看？',
+                    topicImage:'//user-gold-cdn.xitu.io/2018/7/12/1648defb2fa34ce1?imageView2/1/w/200/h/200/q/85/format/webp/interlace/1',
+                    topicCount:0,
+                    followedNum:1805,
+                    topicNum:110,
+                    isFollowed:true
+                },
+                {
+                    id:'000002',
+                    topicName:'今天学到了',
+                    topicImage:'//user-gold-cdn.xitu.io/2018/5/27/163a0f36c521fec1?imageView2/1/w/200/h/200/q/85/format/webp/interlace/1',
+                    topicCount:0,
+                    followedNum:3726,
+                    topicNum:1990,
+                    isFollowed:true
+                },
+                {
+                    id:'000003',
+                    topicName:'一图胜千言',
+                    topicImage:'//user-gold-cdn.xitu.io/2018/5/15/1635f8400e159843?imageView2/1/w/200/h/200/q/85/format/webp/interlace/1',
+                    topicCount:20,
+                    followedNum:3692,
+                    topicNum:2173,
+                    isFollowed:true
+                }
+            ]
+        }
     }
     render() {
         return (
-            <div className="topic-container">
-                话题页面内容
-            </div>
+            <article className="topic-container">
+                <section className='title'>我关注的话题</section>
+                <section className='content'>
+                    {this.state.topicList.map(item=>{
+                        return <TopicItem key={item.id} item={item} />
+                    })}
+                </section>
+            </article>
         );
     }
 }
