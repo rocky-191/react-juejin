05e683a9f1e3919f085fc1f3f804baa24a298263
rocky-191 <1375022007@qq.com>
2019-04-04 09:42:39 +0800

    将class组件替换为函数式组件

--- src/components/TopicCard/index.js
+++ src/components/TopicCard/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Card,Avatar,List } from 'antd';
 import './self.less';
 
--- src/view/home/HomeNav.js
+++ src/view/home/HomeNav.js
@@ -1,22 +1,34 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { NavLink } from 'react-router-dom'
 import './home.less';
-class HomeNav extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {}
-    }
-    render() {
-        return (
-            <ul>
-                {this.props.tags.map((item,index)=>{
-                    return <li key={item.path}>
-                        <NavLink to={`/timeline/${item.path}`}>{item.text}</NavLink>
-                    </li>
-                })}
-            </ul>
-        );
-    }
+// class HomeNav extends Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {}
+//     }
+//     render() {
+//         return (
+//             <ul>
+//                 {this.props.tags.map((item,index)=>{
+//                     return <li key={item.path}>
+//                         <NavLink to={`/timeline/${item.path}`}>{item.text}</NavLink>
+//                     </li>
+//                 })}
+//             </ul>
+//         );
+//     }
+// }
+
+function HomeNav(props){
+    return (
+        <ul>
+            {props.tags.map((item,index)=>{
+                return <li key={item.path}>
+                    <NavLink to={`/timeline/${item.path}`}>{item.text}</NavLink>
+                </li>
+            })}
+        </ul>
+    );
 }
 
 export default HomeNav;
