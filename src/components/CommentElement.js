bcef88150985cb16e44d076394c0ae5bce872823
rocky-191 <1375022007@qq.com>
2019-04-04 09:53:44 +0800

    细节优化，类组件改为函数式组件

--- src/components/CommentElement.js
+++ src/components/CommentElement.js
@@ -1,42 +1,73 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { List, Avatar, Icon } from 'antd';
 import timeUtil from '@/utils/setTime';
 
-class CommentElement extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {};
-    }
-    render() {
-        const IconText = ({ type, text }) => (
-            <span>
-                <Icon type={type} style={{ marginRight: 8 }} />
-                {text}
-            </span>
-        );
-        return (
-            <div>
-                <List
-                    itemLayout="vertical"
-                    size="large"
-                    dataSource={this.props.commentList}
-                    renderItem={item => (
-                    <List.Item
-                        key={item.userId}
-                        actions={[<span>{timeUtil.getTimeAgo(item.editDate)}</span>,<IconText type="like-o" text={item.starNum} />, <IconText type="message" text={item.commentNum} />]}
-                    >
-                        <List.Item.Meta
-                        avatar={<a href={'/user/'+item.userId}><Avatar src={item.userImage} /></a>}
-                        title={<div>{item.authorName}</div>}
-                        description={item.userDesc}
-                        />
-                        {item.commentText}
-                    </List.Item>
-                    )}
-                />
-            </div>
-        );
-    }
+// class CommentElement extends Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {};
+//     }
+//     render() {
+//         const IconText = ({ type, text }) => (
+//             <span>
+//                 <Icon type={type} style={{ marginRight: 8 }} />
+//                 {text}
+//             </span>
+//         );
+//         return (
+//             <div>
+//                 <List
+//                     itemLayout="vertical"
+//                     size="large"
+//                     dataSource={this.props.commentList}
+//                     renderItem={item => (
+//                     <List.Item
+//                         key={item.userId}
+//                         actions={[<span>{timeUtil.getTimeAgo(item.editDate)}</span>,<IconText type="like-o" text={item.starNum} />, <IconText type="message" text={item.commentNum} />]}
+//                     >
+//                         <List.Item.Meta
+//                         avatar={<a href={'/user/'+item.userId}><Avatar src={item.userImage} /></a>}
+//                         title={<div>{item.authorName}</div>}
+//                         description={item.userDesc}
+//                         />
+//                         {item.commentText}
+//                     </List.Item>
+//                     )}
+//                 />
+//             </div>
+//         );
+//     }
+// }
+
+function CommentElement({commentList}){
+    const IconText = ({ type, text }) => (
+        <span>
+            <Icon type={type} style={{ marginRight: 8 }} />
+            {text}
+        </span>
+    );
+    return (
+        <div>
+            <List
+                itemLayout="vertical"
+                size="large"
+                dataSource={commentList}
+                renderItem={item => (
+                <List.Item
+                    key={item.userId}
+                    actions={[<span>{timeUtil.getTimeAgo(item.editDate)}</span>,<IconText type="like-o" text={item.starNum} />, <IconText type="message" text={item.commentNum} />]}
+                >
+                    <List.Item.Meta
+                    avatar={<a href={'/user/'+item.userId}><Avatar src={item.userImage} /></a>}
+                    title={<div>{item.authorName}</div>}
+                    description={item.userDesc}
+                    />
+                    {item.commentText}
+                </List.Item>
+                )}
+            />
+        </div>
+    );
 }
 
 export default CommentElement;
--- src/components/PersonalPop/index.js
+++ src/components/PersonalPop/index.js
@@ -1,39 +1,67 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Avatar,Button } from 'antd';
 import './self.less'
 
-class PersonalPop extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {  };
-    }
-    render() {
-        const { id,avatar,author,description,following,follower,isFollowed}=this.props.info
-        return (
-            <div className='personalPop'>
-                <div className='part1'>
-                    <Avatar size={60} src={avatar} />
-                    <div style={{marginLeft:'15px',fontSize:'14px'}}>
-                        <p style={{color:'#333'}}>{author}</p>
-                        <p style={{color:'#999'}}>{description}</p>
-                    </div>
+// class PersonalPop extends Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {  };
+//     }
+//     render() {
+//         const { id,avatar,author,description,following,follower,isFollowed}=this.props.info
+//         return (
+//             <div className='personalPop'>
+//                 <div className='part1'>
+//                     <Avatar size={60} src={avatar} />
+//                     <div style={{marginLeft:'15px',fontSize:'14px'}}>
+//                         <p style={{color:'#333'}}>{author}</p>
+//                         <p style={{color:'#999'}}>{description}</p>
+//                     </div>
+//                 </div>
+//                 <ul>
+//                     <li>
+//                         <p className='num'>{following}</p>
+//                         <p style={{color:'#999'}}>关注</p>
+//                     </li>
+//                     <li>
+//                         <p className='num'>{follower}</p>
+//                         <p style={{color:'#999'}}>关注者</p>
+//                     </li>
+//                     <li>
+//                         <Button onClick={()=>this.props.handleFollow(id)}>{isFollowed?'已关注':'关注'}</Button>
+//                     </li>
+//                 </ul>
+//             </div>
+//         );
+//     }
+// }
+
+function PersonalPop({info,handleFollow}){
+    const { id,avatar,author,description,following,follower,isFollowed}=info
+    return (
+        <div className='personalPop'>
+            <div className='part1'>
+                <Avatar size={60} src={avatar} />
+                <div style={{marginLeft:'15px',fontSize:'14px'}}>
+                    <p style={{color:'#333'}}>{author}</p>
+                    <p style={{color:'#999'}}>{description}</p>
                 </div>
-                <ul>
-                    <li>
-                        <p className='num'>{following}</p>
-                        <p style={{color:'#999'}}>关注</p>
-                    </li>
-                    <li>
-                        <p className='num'>{follower}</p>
-                        <p style={{color:'#999'}}>关注者</p>
-                    </li>
-                    <li>
-                        <Button onClick={()=>this.props.handleFollow(id)}>{isFollowed?'已关注':'关注'}</Button>
-                    </li>
-                </ul>
             </div>
-        );
-    }
+            <ul>
+                <li>
+                    <p className='num'>{following}</p>
+                    <p style={{color:'#999'}}>关注</p>
+                </li>
+                <li>
+                    <p className='num'>{follower}</p>
+                    <p style={{color:'#999'}}>关注者</p>
+                </li>
+                <li>
+                    <Button onClick={()=>handleFollow(id)}>{isFollowed?'已关注':'关注'}</Button>
+                </li>
+            </ul>
+        </div>
+    );
 }
 
 export default PersonalPop;
